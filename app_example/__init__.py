b45c648de33a0cf5d28a12c43f00a6ad126bb3bd
nicktrejo <43823279+nicktrejo@users.noreply.github.com>
2018-10-24 11:11:08 +0200

    subido lo del profe

---
+++ app_example/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask, session
+import os
+import sys
+
+app = Flask(__name__)
+
+# ejemplo de sesion Flask: http://flask.pocoo.org/docs/1.0/quickstart/#sessions
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+try:
+    from flask_session import Session
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    SESSION_TYPE = 'filesystem'
+    SESSION_FILE_DIR = this_dir + '/thesessions'
+    SESSION_COOKIE_NAME = 'flasksessionid'
+    app.config.from_object(__name__)
+    #app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+    Session(app)
+    print >>sys.stderr, "Usando sesiones de Flask-Session en fichero del servidor"
+except ImportError as e:
+    print >>sys.stderr, "Flask-Session no disponible, usando sesiones de Flask en cookie"
+
+
+from app_example import routes
---
+++ app_example/routes.py
@@ -0,0 +1,32 @@
+from app_example import app
+from flask import render_template, request, url_for, redirect, session
+import json
+import os
+import sys
+
+@app.route('/')
+@app.route('/index')
+def index():
+    print >>sys.stderr, url_for('static', filename='estilo.css')
+    catalogue_data = open(os.path.join(app.root_path,'catalogue/catalogue.json')).read()
+    catalogue = json.loads(catalogue_data)
+    return render_template('index.html', title = "Home", movies=catalogue['peliculas'])
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # doc sobre request object en http://flask.pocoo.org/docs/1.0/api/#incoming-request-data
+    if 'username' in request.form:
+        # aqui se deberia validar con fichero .dat del usuario
+        if request.form['username'] == 'pp':
+            session['usuario'] = request.form['username']
+            # se puede usar request.referrer para volver a la pagina desde la que se hizo login
+            return redirect(url_for('index'))
+        else:
+            # aqui se le puede pasar como argumento un mensaje de login invalido
+            return render_template('login.html', title = "Sign In")
+    else:
+        # se puede guardar la pagina desde la que se invoca 
+        session['url_origen']=request.referrer
+        # print a error.log de Apache si se ejecuta bajo mod_wsgi
+        print >>sys.stderr, request.referrer
+        return render_template('login.html', title = "Sign In")
